d4e1a26f71ddcc32342d6df12d6a78578de04f8c
jessicaHY <jessica@heiyan.com>
2014-04-29 14:02:57 +0800

    init

--- cloud/main.js
+++ cloud/main.js
@@ -1,5 +1,5 @@
-//require("cloud/app.js");
-require("cloud/services/pay.js")
+require("cloud/app.js");
+//require("cloud/services/pay.js")
 // Use AV.Cloud.define to define as many cloud functions as you want.
 // For example:
 AV.Cloud.define("getIcon", function(req, res) {
